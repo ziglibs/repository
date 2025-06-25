5e5e37c0db4a2c60ac26ca5cfbde45d9ad11525e
flopetautschnig (floscodes) <mail@floscodes.net>
2025-06-25 13:00:22 +0200

    fix build.zig for newer zig versions

--- build.zig
+++ build.zig
@@ -1,17 +1,24 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.resolveTargetQuery(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     const verifier = b.addExecutable(.{
         .name = "verifier",
-        .root_source_file = .{ .path = "tools/verifier.zig" },
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = .{ .src_path = .{ .owner = b, .sub_path = "tools/verifier.zig" } },
     });
     const adder = b.addExecutable(.{
         .name = "adder",
-        .root_source_file = .{ .path = "tools/adder.zig" },
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = .{ .src_path = .{ .owner = b, .sub_path = "tools/adder.zig" } },
     });
 
-    const verify = verifier.run();
-    const add = adder.run();
+    const verify = b.addRunArtifact(verifier);
+    const add = b.addRunArtifact(adder);
 
     const verify_step = b.step("verify", "Verifies if the repository structure is sane and valid.");
     verify_step.dependOn(&verify.step);
@@ -19,18 +26,25 @@ pub fn build(b: *std.Build) void {
     const add_step = b.step("add", "Adds a new package");
     add_step.dependOn(&add.step);
 
-    try buildToolsFixer(b);
+    try buildToolsFixer(b, target, optimize);
 }
 
-fn buildToolsFixer(b: *std.build.Builder) !void {
+fn buildToolsFixer(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+) !void {
     const exe = b.addExecutable(.{
         .name = "fix",
-        .root_source_file = .{ .path = "tools/fixer.zig" },
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = .{ .src_path = .{ .owner = b, .sub_path = "tools/fixer.zig" } },
     });
-    exe.linkSystemLibrary("libcurl");
-    exe.linkLibC();
-    const run_cmd = exe.run();
 
+    exe.linkSystemLibrary("curl");
+    exe.linkLibC();
+
+    const run_cmd = b.addRunArtifact(exe);
     const run_step = b.step("fix", "Fix GitHub package metadata");
     run_step.dependOn(&run_cmd.step);
 }
